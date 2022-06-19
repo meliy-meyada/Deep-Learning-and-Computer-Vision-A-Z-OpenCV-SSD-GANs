fa2ce8f6fb2aa32a2cc0732ec6b316f4165f758f
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-06-19 16:48:27 +0700

    Detections frame

--- Detect_Object/object_detection.py
+++ Detect_Object/object_detection.py
@@ -14,4 +14,21 @@ def detect(frame, net, transform):
     frame_t = transform(frame)[0]
     x = torch.from_numpy(frame_t).permute(2, 0, 1)
     x = Variable(x.unsqueeze(0))
-    
+    y = net(x)
+    detections = y.data
+    scale = torch.Tensor([width, height, width, height])
+    # detections = [batch, number of classes, number of occurence, {score, x0, y0, x1, y1}]
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.6:
+            pt = (detections[0, i, j, 1:] * scale).numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
+    return frame
+
+
+
+
+
+
