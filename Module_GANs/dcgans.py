da4de210c5dd80fa374bee29e9afb5d49887e5ec
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-06-20 13:17:58 +0700

    create transformations

--- Module_GANs/dcgans.py
+++ Module_GANs/dcgans.py
@@ -25,6 +25,8 @@ from torch.autograd import Variable
 batchSize = 64
 imageSize = 64
 
+# Create the transformations
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),])
 
 
 
